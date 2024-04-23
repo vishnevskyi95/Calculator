c6d003f9bfdba8ae244ff1d66a73e6551e38f867
Gleb <vishnyagleb2@gmail.com>
2024-04-24 00:37:35 +0300

    Добавьте файлы проекта.

---
+++ Calc_104_task.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+
+
+
+char getUserOperator() {
+	char userOperator;
+	do
+	{
+		std::cout << "Enter operator(+,-,* or /): ";
+		std::cin >> userOperator;
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+		else
+		{
+			std::cin.ignore(32767, '\n');
+		}
+		
+
+	} while (userOperator != '+' && userOperator != '-'&& userOperator != '*'&& userOperator != '/');
+	return userOperator;
+}
+
+int getUserNum() {
+	int userNum;
+	do
+	{
+		std::cout << "Enter number: ";
+		std::cin >> userNum;
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+		else
+		{
+			std::cin.ignore(32767, '\n');
+			return userNum;
+		}
+
+	} while (true);
+	
+}
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int subtract(int a, int b) {
+	return a - b;
+}
+
+int multiply(int a, int b) {
+	return a * b;
+}
+int divide(int a, int b) {
+	return a / b;
+}
+
+typedef int (*arithmeticFcn)(int, int);
+
+struct arithmeticStruct
+{
+	char op;
+	arithmeticFcn fcn;
+};
+
+static arithmeticStruct arithmeticArray[]{
+	{ '+', add },
+	{ '-', subtract },
+	{ '*', multiply },
+	{ '/', divide }
+};
+
+arithmeticFcn getArithmeticFcn(char op) {
+	for (auto& arith : arithmeticArray)
+	{
+		if (arith.op == op)
+			return arith.fcn;
+	}
+	return add;
+}
+
+int main()
+{
+	int a = getUserNum();
+	char op = getUserOperator();
+	int b = getUserNum();
+	arithmeticFcn fcn = getArithmeticFcn(op);
+	std::cout << a << ' ' << op << ' ' << b << " = " << fcn(a, b) << '\n';
+
+	
+}
